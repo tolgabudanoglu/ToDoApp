fe2db99201178f1b170ed55915e8eb7a3c88b3a6
tolga budanoğlu <budanoglu.tolga@gmail.com>
2023-09-27 21:36:10 +0300

    setup database

---
+++ app/src/main/java/com/example/todoapp/data/TypeConverter.kt
@@ -0,0 +1,18 @@
+package com.example.todoapp.data
+
+
+import androidx.room.TypeConverter
+
+class TypeConverter {
+
+    @TypeConverter
+    fun fromPriority(priority: Priority): String {
+        return priority.name
+    }
+
+    @TypeConverter
+    fun toPriority(priority: String): Priority {
+        return Priority.valueOf(priority)
+    }
+
+}
